47768cfc9800cf0d1a0ac116ab4e243c640bce7d
Nathan <natoucs@users.noreply.github.com>
2017-03-02 09:23:43 +0000

    Create datastruct.fsx

---
+++ datastruct.fsx
@@ -0,0 +1,22 @@
+ module MachineState =
+        type Register = R of int
+        type Memory = M of int  
+        type RunState = 
+        | RunOK
+        | RunTimeErr of string
+        | SyntaxErr of string
+        type Flags = 
+            {
+                N: bool
+                Z: bool
+                C: bool
+                V: bool
+            }
+        type MachineState = 
+            { 
+                RegMap : Map<Register, int>
+                MemMap : Map<Memory, int> 
+                Flags : Flags 
+                State : RunState 
+            }
+            
