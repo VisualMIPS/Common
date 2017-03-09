52593392d8e6397e33db43dc4573372caefd9f0c
Ben Withers <manicben20@gmail.com>
2017-03-09 02:03:51 +0000

    Tokeniser/Parser placeholder

---
+++ parser.fsx
@@ -0,0 +1,25 @@
+﻿(*  Tokeniser
+    VisualMIPS 2017
+*)
+
+module Parser =
+    // Placeholder functions from Tick4, will be replaced.
+    // Need Error responses for different parsing situations
+    type parseResponse =
+        | VarValue of int // for commands that return data
+        | ParseError // if command string is invalid
+        | OK // for valid commads that return no data
+    
+    open System.Text.RegularExpressions
+    // Need alpha (for function names) and num (for values) to identify valid tokens
+    let isAlpha s = Regex.IsMatch (s, @"^[a-zA-Z]+$")
+
+    let isNum s = Regex.IsMatch (s, @"^-?[0-9]+$")
+    // Need parse function to take in tokens and output function handle and data
+    // Functions can be listed in map and indexed by name, e.g. Map<name of String, function> -> <"add", addFunction>
+    let parse (tokens: string[]) =            
+        match tokens.[0] with
+        | "READ" when tokens.Length = 2 && isAlpha tokens.[1] -> readVar tokens.[1]
+        | "WRITE" when tokens.Length = 3 && isAlpha tokens.[1] && isNum tokens.[2] -> writeVar tokens.[1] (int tokens.[2])
+        | "ASSIGN" when tokens.Length = 3 && isAlpha tokens.[1] && isAlpha tokens.[2] -> assignVar tokens.[1] tokens.[2]
+        | _ -> ParseError
---
+++ tokeniser.fsx
@@ -0,0 +1,14 @@
+﻿(*  Tokeniser
+    VisualMIPS 2017
+*)
+
+module Tokeniser =
+    // Placeholder functions from Tick4, will be replaced.
+    // Need Error responses for different situations.
+    type tokenResponse =
+        | VarValue of int // for commands that return data
+        | TokeniseError // if command string is invalid
+        | OK // for valid commads that return no data
+    // Need tokenise function to return tokens to parse
+    let tokenise (s: string) = 
+        Array.filter ((<>) "") (s.Split(' ' , '\t','\n','\r','\f'))  
