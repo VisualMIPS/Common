93ff12de58bc26a6e254e3eb7560ff515b411027
natoucs <nathan.cavaglione@gmail.com>
2017-03-11 21:40:23 +0000

    test

--- VisualMIPS/instructer.fs
+++ VisualMIPS/instructer.fs
@@ -19,12 +19,28 @@ let executeIType (mach : MachineState, I_type : instr) =
 let executeRType (mach : MachineState, R_type : instr) =
     let rS = mach.getReg instr.r_s
     let rT = mach.getReg instr.r_t
-    
     match R_Type.opcode with
     | ADD -> opADD mach instr rS rT
+    | ADDU ->
+    | AND -> opAND mach instr rS rT //repetitive, can do better, with a curried function or smg
+    | OR -> opOR mach instr rS rT
+    | SRA -> opSRA mach instr rS rT
+    | SRAV | SRL | SRLV | SLL | SLLV | SUB | SUBU 
+    | XOR -> opXOR mach instr rS rT
+    | SLT | SLTU | DIV | DIVU | MULT | MULTU 
+    | JR | JALR | MFHI | MFLO | MTHI | MTLO
 
-
+let opADD (mach: MachineState, R_type: instr, Word rS, Word rT) = // to be coded
 
 let opAND (mach: MachineState, R_type: instr, Word rS, Word rT) =
     mach.setReg instr.r_d (rS &&& rT)
+
+let opOR (mach: MachineState, R_type: instr, Word rS, Word rT) =
+    mach.setReg instr.r_d (rS ||| rT)
+
+let opSRA (mach: MachineState, R_type: instr, Word rS, Word rT) =
+      mach.setReg instr.r_d (rS >>> instr.shift) //should pass shift already retrieved ?
+
+let opXOR (mach: MachineState, R_type: instr, Word rS, Word rT) =
+    mach.setReg instr.r_d (rS ^^^ rT)
     
