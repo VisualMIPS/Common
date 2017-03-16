4b3a65b718d95d68f3285f8fdd3f937c36537e33
natoucs <nathan.cavaglione@gmail.com>
2017-03-16 15:43:33 +0000

    simpleR and shiftR implemented,not tested 2

--- VisualMIPS/instructer.fs
+++ VisualMIPS/instructer.fs
@@ -29,20 +29,54 @@ module Executor =
         setNextNextPC (Word ((getNextPC mach |> T.getValue) + 4u*(uint32 immed))) //need to sign extend when converting to uint32
 
     let processSimpleR (instr: Instruction) (mach : MachineState) =
-        let localMap = Map [(AND,opAND);(OR, opOR); (SRAV,opSRAV); (XOR, opXOR);]
-        let rs = getReg instr.rs mach
-        let rt = getReg instr.rt mach
-        let fn = Map.find instr.opcode localMap
-        let output = fn mach instr rs rt
+        let rs = T.getValue(getReg instr.rs mach)
+        let rt = T.getValue(getReg instr.rt mach)
+        let tmp = 
+             match instr.opcode with
+             | ADDU -> rs + rt
+             | AND -> rs &&& rt
+             | OR -> rs ||| rt
+             | SRAV -> uint32(int32 rt >>> int32 rs)
+             | SRLV -> rt >>> int32 rs
+             | SLLV -> rt <<< int32 rs
+             | SUBU -> rs - rt
+             | XOR -> rs ^^^ rt
+             | SLT -> 
+                match int32(rs) < int32(rt) with
+                | true -> 1u 
+                | false -> 0u
+             | SLTU ->
+                match rs < rt with
+                | true -> 1u
+                | false -> 0u
+             | MFHI -> T.getValue(mach.Hi)
+             | MFLO -> T.getValue(mach.Lo)
+             | _ -> failwith "opcode does not belong to processSimpleR functions"
+        let output = Word(tmp)
         setReg instr.rd output mach
     
     let processShiftR (instr : Instruction) (mach : MachineState) =
-        let localMap = Map [(SRA, opSRA)]
+        let rt = T.getValue(getReg instr.rt mach)
+        let shiftval = int32 (T.getValue(instr.shift))
+        let tmp = 
+            match instr.opcode with 
+            | SRA -> uint32 (int32 rt >>> shiftval)
+            | SRL -> rt >>> shiftval
+            | SLL -> rt <<< shiftval
+            | _ -> failwith "opcode does not belong to processShiftR functions"
+        let output = Word(tmp)
+        setReg instr.rd output mach
+    
+   (* let processFullR (instr: Instruction) (mach : MachineState) =
+        let localMap = Map [(ADD, opADD)]
         let rs = getReg instr.rs mach
         let rt = getReg instr.rt mach
         let fn = Map.find instr.opcode localMap
-        let output = fn mach instr rs rt instr.shift
-        setReg instr.rd output mach
+        let output = fn mach instr rs rt 
+    *)
+
+    
+    // --------------- //
 
     let opTypeMap = Map [([DIV; DIVU; MULT; MULTU],processMultDiv);
                         ([MFHI; MFLO; MTHI; MTLO],processHILO);
--- VisualMIPS/r_type.fs
+++ VisualMIPS/r_type.fs
@@ -5,69 +5,6 @@ module Rtypes =
     open Instructions
     open MachineState
     
-    //simpleR functions --> convert to match statement later as repetitive
-    // + no need to pass MachineState vrbl as unused in all simpleR functions
 
     //bear in mind that when a user writes a negative number, 
     // it is stored as a 2's complement in the uint32 type
-
-    let ADDU (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        Word (rS + rT)
-
-    let opAND (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        Word (rS &&& rT)
-
-    let opOR (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        Word (rS ||| rT)
-
-    let opSRAV (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        // signed : duplicating the sign-bit (bit 31) in the emptied bits
-        // converting rT to int makes the bitwise operator treat it as a signed nbr
-        Word ( uint32(int32 rT >>> int32 rS) )
-
-    let opSRLV (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        // unsigned : inserting zeros into the emptied bits
-        // rT is uint so it will put 0 automatically
-        Word (rT >>> int32 rS)
-
-    let opSLLV (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        // inserting zeros into the emptied bits
-        Word (rT <<< int32 rS)
-    
-    let opSUBU (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        Word (rS - rT)
-
-    let opXOR (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        Word (rS ^^^ rT)
-
-    let opSLT (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        //signed comparison -> convert as signed nbr to treat them as so
-        match int32(rS) < int32(rT) with
-        | true -> Word(1u) //1u = uint32 1
-        | false -> Word(0u)
-    
-    let opSLTU (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        //unsigned comparison, no pre-processing on rT or rS
-        match rS < rT with
-        | true -> Word(1u)
-        | false -> Word(0u)
-    
-    let opMFHI (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        //useless rs and rt, just returns HI
-        mach.Hi
-
-    let opMFLO (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) =
-        //just returns LO
-        mach.Lo
-
-    //shiftR functions
-
-    let opSRA (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) (Shiftval shiftval) =
-        //does not need rS passed to it
-        Word( uint32 (int32 rT >>> int32 shiftval) )
-    
-    let opSRL (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) (Shiftval shiftval) =
-        Word( rT >>> int32 shiftval )
-
-    let opSLL (mach: MachineState) (instr : Instruction) (Word rS) (Word rT) (Shiftval shiftval) =
-        Word( rT <<< int32 shiftval )
