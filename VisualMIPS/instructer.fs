d10b6c7fcf5a85ae4307e70c8e919a3b63641e9e
natoucs <nathan.cavaglione@gmail.com>
2017-03-17 00:05:55 +0000

    finishing implementation of fullR

--- VisualMIPS/instructer.fs
+++ VisualMIPS/instructer.fs
@@ -8,27 +8,8 @@ module Executor =
     open Rtypes
     open Itypes
 
-    let processMultDiv (instr: Instruction)  (mach : MachineState)= failwith "Not Implemented"
 
-    let processHILO (instr: Instruction) (mach : MachineState) = failwith "Not Implemented"
-
-    let processBranchI (instr: Instruction) (mach : MachineState) =
-        let immed = (T.getValue instr.immed)
-        let rs = T.getValue( getReg instr.rs mach )
-        let rt = T.getValue( getReg instr.rt mach )//Only used in a couple.
-        let (branch, link) = match instr.opcode with
-                                | BGEZ when rs >= 0u -> (true,false)
-                                | BGEZAL when rs >= 0u -> (true,true)
-                                | BEQ when rs = rt -> (true,false)
-                                | BNE when rs <> rt -> (true,false)
-                                | BLEZ when rs <= 0u -> (true, false)
-                                | BLTZ when rs < 0u -> (true, false)
-                                | BLTZAL when rs < 0u -> (true, true)
-                                | BGTZ when rs >= 0u -> (true, false)
-                                //FIXME: Do the link commands always link? Spec seems to suggest that.
-                                | _ -> (false, false)
-        setNextNextPC (Word ((getNextPC mach |> T.getValue) + 4u*(uint32 immed))) //need to sign extend when converting to uint32
-
+    // R execution
     let processSimpleR (instr: Instruction) (mach : MachineState) =
         let rs = T.getValue(getReg instr.rs mach)
         let rt = T.getValue(getReg instr.rt mach)
@@ -86,14 +67,52 @@ module Executor =
                 let newmach = fn2 mach instr rs rt
                 newmach 
         returnmach 
+    
+    let processMultDiv (instr: Instruction)  (mach : MachineState)= failwith "Not Implemented"
 
+    let processHILO (instr: Instruction) (mach : MachineState) = failwith "Not Implemented"
 
+    let processBranchI (instr: Instruction) (mach : MachineState) =
+        let immed = (T.getValue instr.immed)
+        let rs = T.getValue( getReg instr.rs mach )
+        let rt = T.getValue( getReg instr.rt mach )//Only used in a couple.
+        let (branch, link) = match instr.opcode with
+                                | BGEZ when rs >= 0u -> (true,false)
+                                | BGEZAL when rs >= 0u -> (true,true)
+                                | BEQ when rs = rt -> (true,false)
+                                | BNE when rs <> rt -> (true,false)
+                                | BLEZ when rs <= 0u -> (true, false)
+                                | BLTZ when rs < 0u -> (true, false)
+                                | BLTZAL when rs < 0u -> (true, true)
+                                | BGTZ when rs >= 0u -> (true, false)
+                                //FIXME: Do the link commands always link? Spec seems to suggest that.
+                                | _ -> (false, false)
+        setNextNextPC (Word ((getNextPC mach |> T.getValue) + 4u*(uint32 immed))) //need to sign extend when converting to uint32
+
+    // I execution
+
+    let processSimpleI (instr: Instruction) (mach : MachineState) =
+        let rs = T.getValue(getReg instr.rs mach)
+        let rt = T.getValue(getReg instr.rt mach)
+        let immediate = uint32(T.getValue instr.immed) //pads with 16-bit of MSB's value
+        let tmp = 
+             match instr.opcode with
+             | ADDIU -> rs + immediate //pad with MSB ????
+             | ANDI -> rs &&& immediate //pad with zeros
+             | ORI -> rs ||| immediate
+             | XORI -> rs ^^^ immediate
+             | _ -> failwith "opcode does not belong to processSimpleI functions"
+        let output = Word(tmp)
+        setReg instr.rt output mach
     // --------------- //
+
+    // Dispatch execution
 
     let opTypeMap = Map [
                         ([ADDU; AND; OR; SRAV; SRLV; SLLV; SUBU; XOR; SLT; SLTU; MFHI; MFLO], processSimpleR);
                         ([SRA; SRL; SLL], processShiftR);
                         ([ADD; SUB; JALR; DIV; DIVU; MULT; MULTU; JR; MTHI; MTLO], processFullR);
+                        ([ADDIU; ANDI; ORI; XORI],processSimpleI);
                         ([DIV; DIVU; MULT; MULTU],processMultDiv);
                         ([MFHI; MFLO; MTHI; MTLO],processHILO);
                         ]
